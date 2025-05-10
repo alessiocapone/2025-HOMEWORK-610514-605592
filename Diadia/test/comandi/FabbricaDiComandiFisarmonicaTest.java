0ea73922aab5aada60aa9a45b6034a46ae77f824
vladUR3 <vla.kapatsyna@stud.uniroma3.it>
2025-05-10 22:38:10 +0200

    Create FabbricaDiComandiFisarmonicaTest.java

---
+++ Diadia/test/comandi/FabbricaDiComandiFisarmonicaTest.java
@@ -0,0 +1,49 @@
+package it.uniroma3.diadia.comandi;
+
+import static org.junit.jupiter.Assert.assertEquals;
+
+import org.junit.jupiter.AfterEach;
+import org.junit.jupiter.BeforeEach;
+import org.junit.jupiter.Test;
+
+import it.uniroma3.diadia.IOConsole;
+import it.uniroma3.diadia.IO;
+
+public class FabbricaDiComandiFisarmonicaTest {
+
+	private FabbricaDiComandiFisarmonica fabbrica;
+	private IO io;
+	private Comando expected;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		io = new IOConsole();
+		fabbrica = new FabbricaDiComandiFisarmonica(io);
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testComandoNonValido() {
+		expected = new ComandoNonValido();
+		assertEquals( expected.getNome(), fabbrica.costruisciComando("pippo").getNome());
+	}
+	
+	@Test
+	public void testComandoConParametro() {
+		expected = new ComandoVai();
+		expected.setParametro("nord");
+		Comando current = fabbrica.costruisciComando("vai nord");
+		assertEquals( expected.getNome(), current.getNome());
+		assertEquals( expected.getParametro(), current.getParametro());
+	}
+	
+	@Test
+	public void testComandoSenzaParametro() {
+		expected = new ComandoFine();
+		assertEquals( expected.getNome(), fabbrica.costruisciComando("fine").getNome());
+	}
+
+}
